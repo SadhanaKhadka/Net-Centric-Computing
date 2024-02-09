87599214fec590f04614bc1827a471b095a4cc5a
Sadhana <142109910+SadhanaKhadka@users.noreply.github.com>
2024-02-09 15:30:25 +0545

    Add files via upload

---
+++ PropertyDemo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace NccLabSadhana
+{
+    class Circle
+    {
+        private double radius = 7.5;
+
+        //property
+        /*
+        //While there is nothing to check:
+        public double Radius
+        {
+            get; set;
+        }
+        */
+
+        public double Radius
+        {
+            //get accessor
+            get
+            {
+                if (radius > 0)
+                    return radius;
+                else return 0;
+            }
+            //set acessor
+            set
+            {
+                radius = value;
+            }
+
+        }
+        /*//getter method
+        public double GetRadius()
+        {
+            return radius;
+        }
+        //setter method
+        public void SetRadius(double radius)
+        {
+            this.radius = radius;
+        }*/
+    }
+    internal class PropertyDemo
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            /*c1.SetRadius(12.5); //used for setter
+            Console.WriteLine("value of radius=" + c1.radius);
+            Console.WriteLine("value of radius=" + c1.GetRadius());//used for getter */
+            c1.Radius = 12.5;
+            Console.WriteLine("value of radius=" + c1.Radius);
+        }
+
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace NccLabSadhana
+{
+    class Box
+    {
+        public static int count; //static  variable
+        double length, breadth, height;
+
+        //constructor
+        public Box()
+        {
+            length = 6.5;
+            breadth = 4.5;
+            height = 5.5;
+            count++;
+        }
+        public Box(double length, double breadth, double height)
+        {
+            this.length = length;
+            this.breadth = breadth;
+            this.height = height;
+            count++;
+        }
+
+        public void SetDimension(double l, double b, double h)
+        {
+            length = l;
+            breadth = b;
+            height = h;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length=" + length + "Breadth=" + breadth + "Height=" + height);
+        }
+
+        public static void ShowCount()
+        {
+            Console.WriteLine("No. of objects created=" + Box.count);
+
+        }
+    }
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Box.ShowCount();
+            //Box box1;
+            Box box1 = new Box();
+            Box box2 = new Box(34.5, 6.9, 7.7);
+            box1.Display();
+            box1.SetDimension(33.5, 56.8, 99.1);
+            box2.Display();
+            //Console.WriteLine("No. of objects created=" + Box.count);
+            Box.ShowCount();
+        }
+    }
+}

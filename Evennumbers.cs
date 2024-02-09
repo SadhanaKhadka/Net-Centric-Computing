a62f5ddd637368c989c912d583f6e5fec73094dc
Sadhana <142109910+SadhanaKhadka@users.noreply.github.com>
2024-02-09 16:30:02 +0545

    Add files via upload

---
+++ Evennumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NccLabSadhana
+{
+    internal class Evennumbers
+    {
+        internal class Array
+        {
+            static void Main(string[] args)
+            {
+                int[] array = new int[30];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = (i + 1) * 2;
+                    Console.WriteLine("array [" + i + "] = " + array[i]);
+                }
+            }
+        }
+    }
+}
